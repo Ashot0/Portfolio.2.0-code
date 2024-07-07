a08a6b6b89b2ca9197746f1b4d180e22d963253b
Ashot0 <illia.holovan@gmail.com>
2024-07-07 12:36:02 +0300

    ScrollToTopUpdate

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -8,10 +8,11 @@
 </template>
 
 <script>
-import { onMounted, onUnmounted, computed } from 'vue';
+import { onMounted, onUnmounted, computed, watch } from 'vue';
 import { useStore } from 'vuex';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
 import Lenis from 'lenis';
 import AnimatedText from '@/components/AnimatedText/AnimatedText.vue';
 import Hero from '@/components/Hero/Hero.vue';
@@ -30,36 +31,45 @@ export default {
 	},
 	setup() {
 		gsap.registerPlugin(ScrollTrigger);
+		gsap.registerPlugin(ScrollToPlugin);
 		const store = useStore();
 		let lenisGlobal = null;
 		const isLoading = computed(() => store.getters.checkLoading);
-		onMounted(() => {
-			if (isLoading.value) {
-				setTimeout(() => {
-					window.scrollTo(0, 0);
-				}, 300);
-			}
+
+		const initLenis = () => {
 			lenisGlobal = new Lenis({
 				smoothWheel: true,
 				duration: 0.7,
 				wheelMultiplier: 1.2,
 				animatedScroll: 0,
-				easing: (x) => {
-					return Math.sin((x * Math.PI) / 2);
-				},
+				easing: (x) => Math.sin((x * Math.PI) / 2),
 			});
+
 			lenisGlobal.on('scroll', ScrollTrigger.update);
+
 			gsap.ticker.add((time) => {
 				lenisGlobal.raf(time * 1000);
 			});
+
 			gsap.ticker.lagSmoothing(0);
+		};
+
+		onMounted(() => {
+			initLenis();
+			watch(isLoading, (newVal) => {
+				setTimeout(() => {
+					gsap.to(window, { scrollTo: 0, duration: 0 });
+				}, 300);
+			});
 		});
+
 		onUnmounted(() => {
 			if (lenisGlobal) {
 				lenisGlobal.destroy();
 				lenisGlobal = null;
 			}
 		});
+
 		return {};
 	},
 };
